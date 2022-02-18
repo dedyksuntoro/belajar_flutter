4b2d6b411e8ee41fc292ccff375e42da7a380180
itgrb <pt.gemilangrayabersama1@outlook.com>
2022-02-18 15:35:08 +0700

    Init

---
+++ lib/main.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomePage(),
+      theme: ThemeData(
+        primarySwatch: Colors.yellow,
+      ),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  final List bulan = [
+    "Januari",
+    "Fabruari",
+    "Maret",
+    "April",
+    "Mei",
+    "Juni",
+    "Juli",
+    "Agustus",
+    "September",
+    "Oktober",
+    "November",
+    "Desember"
+  ];
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home Page'),
+      ),
+      body: ListView.builder(
+        itemCount: bulan.length,
+        itemBuilder: (context, index) {
+          return Card(
+            child: ListTile(
+              title: Text(bulan[index]),
+              leading: Icon(Icons.calendar_today),
+            ),
+          );
+        }
+      )
+    );
+  }
+}
